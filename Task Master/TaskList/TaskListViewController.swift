dae1e0d1ba070a7dff117a569b6399dfd1d4d0d5
Rahul Anand <rahul.anand@citrix.com>
2024-08-02 23:19:48 +0530

    - Added Add task Controller - Removed ImageView from TableViewCell - Added a selected and Unselected Image to show tasks completed - removed unnecessary code of loading images

--- Task Master/TaskList/TaskListViewController.swift
+++ Task Master/TaskList/TaskListViewController.swift
@@ -11,18 +11,25 @@ class TaskListViewController: UIViewController {
 
     @IBOutlet weak var taskListTableView: UITableView!
     
-    let taskTitleList = ["Fish Food", "Water Plants", "Drink Water", "Cook Food", "Family Time"]
-    let taskDescriptionList = ["Fish Food keeps fishes alive.", "Water keeps Plants healthy, plants also need sunlight.", "Start your day with a glass of Water", "Cook delicious and healthy Food.", "Family Time is the best time."]
+    var taskTitleList = ["Fish Food", "Water Plants", "Drink Water", "Cook Food", "Family Time"]
+    var taskDescriptionList = ["Fish Food keeps fishes alive.", "Water keeps Plants healthy, plants also need sunlight.", "Start your day with a glass of Water", "Cook delicious and healthy Food.", "Family Time is the best time."]
     let taskImageList = ["fish", "plants", "water", "food", "family"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Tasks"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .done, target: self, action: #selector(self.addButtonTapped))
         self.taskListTableView.layer.opacity = 0.85
         self.taskListTableView.dataSource = self
         self.taskListTableView.delegate = self
         let taskCellNib = UINib(nibName: "TaskTableViewCell", bundle: nil)
         self.taskListTableView.register(taskCellNib, forCellReuseIdentifier: "TaskTableViewCell")
+    }
+    
+    @objc func addButtonTapped() {
+        let addTaskViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "AddTaskViewController") as! AddTaskViewController
+        addTaskViewController.delegate = self
+        self.navigationController?.pushViewController(addTaskViewController, animated: true)
     }
 }
 
@@ -34,7 +41,7 @@ extension TaskListViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let taskCell = self.taskListTableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath) as! TaskTableViewCell
-        taskCell.setTaskDetails(title: taskTitleList[indexPath.row], desc: taskDescriptionList[indexPath.row], taskImage: taskImageList[indexPath.row])
+        taskCell.setTaskDetails(title: taskTitleList[indexPath.row], desc: taskDescriptionList[indexPath.row]/*, taskImage: taskImageList[indexPath.row]*/)
         return taskCell
     }
     
@@ -46,3 +53,12 @@ extension TaskListViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
 }
+
+extension TaskListViewController: SaveDetailsDelegate {
+    func saveDetails(title: String, desc: String) {
+        self.taskTitleList.append(title)
+        self.taskDescriptionList.append(desc)
+        self.taskListTableView.reloadData()
+    }
+}
+
